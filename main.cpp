3ea04b9c4b01fafc70de24c98989f65461f3a57c
michaljarnot <m.jarnot@yahoo.com>
2023-04-12 13:53:02 +0200

    feat: implement instance segmentation

--- main.cpp
+++ main.cpp
@@ -1,16 +1,51 @@
-#include "YOLODetector.h"
+#include <iostream>
 
-int main() {
+#include "YOLODetector.h"
+#include "YOLOSegmentor.h"
+
+void InstanceSegmentation(std::string& imagePath) {
+  // Initialize the detector
+  SegNetConfig DetectorConfig = {
+      0.3,
+      0.3,
+      "../models/best-s-640-seg.onnx",
+      "../class.names"};
+  YOLOSegmentor net(DetectorConfig);
+
+  // Initialize the image
+  cv::Mat sourceImage = cv::imread(imagePath);
+
+  // Run detection
+  auto start = std::chrono::steady_clock::now();
+
+  net.Detect(sourceImage);
+
+  auto end = std::chrono::steady_clock::now();
+  auto diff = end - start;
+  std::cout << std::chrono::duration<double, std::milli>(diff).count() << " ms"
+            << std::endl;
+
+  // Show the result
+  static const std::string windowName = "YOLOv5 Instance Segmentation";
+  namedWindow(windowName, cv::WINDOW_NORMAL);
+  imshow(windowName, sourceImage);
+
+  // End
+  cv::waitKey(0);
+  cv::destroyAllWindows();
+}
+
+void ObjectDetection(std::string& imagePath) {
   // Initialize the detector
   NetConfig DetectorConfig = {
       0.3,
       0.5,
       "../models/best-n-640.onnx",
-      "../coco.names"};
+      "../class.names"};
   YOLODetector net(DetectorConfig);
 
   // Initialize the image
-  cv::Mat sourceImage = cv::imread("../samples/game-2.jpg");
+  cv::Mat sourceImage = cv::imread(imagePath);
 
   // Run detection
   auto start = std::chrono::steady_clock::now();
@@ -23,11 +58,22 @@ int main() {
             << std::endl;
 
   // Show the result
-  static const std::string windowName = "YOLO CMake OpenCV ONNX CPP";
+  static const std::string windowName = "YOLOv5 Object Detection";
   namedWindow(windowName, cv::WINDOW_NORMAL);
   imshow(windowName, sourceImage);
 
   // End
   cv::waitKey(0);
   cv::destroyAllWindows();
+}
+
+int main() {
+  std::string imagePath = "../samples/game-1.jpg";
+//  std::string imagePath = "../samples/game-2.jpg";
+//  std::string imagePath = "../samples/game-3.jpg";
+
+  InstanceSegmentation(imagePath);
+  ObjectDetection(imagePath);
+
+  return 0;
 }
